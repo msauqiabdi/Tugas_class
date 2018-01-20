f3bde994429131b4cc3d5c22bed71a84effafda3
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2018-01-20 17:05:37 +0700

    Tambahan

--- MOBIL.cpp
+++ MOBIL.cpp
@@ -8,4 +8,15 @@ class MOBIL{
 	string tipe,nama;
 	
 };
+int main(){
+	
+	MOBIL AVANZA,DAIHATSU,SUZUKI;
+	
+	AVANZA.nama	   =" MOBIL AVANZA ";
+	AVANZA.tipe    =" AVANZA SERI E ";
+	DAIHATSU.nama  =" MOBIL TOYOTA ";
+	DAIHATSU.tipe  =" DAIHATSU AYLA ";
+	SUZUKI.nama    =" MOBIL SUZUKI ";
+	SUZUKI.tipe	   =" SUZUKI ERTIGA ";
+	
 
